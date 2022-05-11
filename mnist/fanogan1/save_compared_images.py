70736c44141c5cb946ec7227804017320d920494
filibp <kasperkiewiczf@gmail.com>
2022-05-11 15:01:13 +0200

    test - delete later

--- mnist/fanogan1/save_compared_images.py
+++ mnist/fanogan1/save_compared_images.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from torchvision.utils import save_image
-
+import pickle
 
 def save_compared_images(opt, datastamp, generator, encoder, dataloader, device):
 
@@ -20,13 +20,15 @@ def save_compared_images(opt, datastamp, generator, encoder, dataloader, device)
 
         real_z = encoder(real_img)
         fake_img = generator(real_z)
-
+        
         compared_images = torch.empty(real_img.shape[0] * 3,
                                       *real_img.shape[1:])
         compared_images[0::3] = real_img
         compared_images[1::3] = fake_img
         compared_images[2::3] = real_img - fake_img
-
+        with open('images.pck','wb') as f:
+            pickle.dump([fake_img, real_img, real_z, compared_images], f)
+        break
         save_image(compared_images.data,
                    "results"+datastamp+f"/images_diff/{opt.n_grid_lines*(i+1):06}.png",
                    nrow=3, normalize=True)
