e592eb8eb5bb3e6c26805a65a0c8d39333dc4f3b
filibp <kasperkiewiczf@gmail.com>
2022-06-08 20:12:52 +0200

    making model smaller

--- mnist/model.py
+++ mnist/model.py
@@ -23,14 +23,14 @@ class Generator(nn.Module):
             return layers
 
         self.model = nn.Sequential(
-            *block(opt.latent_dim, 2048, normalize=False),
+            *block(opt.latent_dim, 1024, normalize=False),
             # *block(512, 1024, normalize=False),
-            # *block(1024, 2048, normalize=False),
+            *block(1024, 2048, normalize=False),
             *block(2048, 4096, normalize=False),
-            *block(4096, 8192, normalize=False),
-            *block(8192, 16384),
+            *block(4096, 8192),
+            # *block(8192, 16384),
             # *block(4096, 8192),
-            nn.Linear(16384, int(np.prod(self.img_shape))),
+            nn.Linear(8192, int(np.prod(self.img_shape))),
             nn.Tanh()
             )
 
@@ -46,10 +46,10 @@ class Discriminator(nn.Module):
         img_shape = (opt.channels, opt.img_size, opt.img_size)
 
         self.features = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 4096),
+            nn.Linear(int(np.prod(img_shape)), 2048),
             nn.LeakyReLU(0.2, inplace=True),
             # nn.Linear(8192, 4096),
-            nn.Linear(4096, 2048),
+            # nn.Linear(4096, 2048),
             nn.Linear(2048, 1024),
             nn.Linear(1024, 512),
             nn.Linear(512, 256),
